c62c85a042d58be9d5564d324755f04ad05040f8
Henry Tochukwu Uzor <h.uzor17@gmail.com>
2024-01-08 19:58:48 +0100

    feat: tsx for lower body of blog page

--- src/components/body/Body.tsx
+++ src/components/body/Body.tsx
@@ -39,6 +39,50 @@ export const Body = () => {
           <p className='postCaption'>Lorem Ipsum Dolor Sit Amet Consectetur. Ut Ut Diam Sit Aliquam Cursus Et Ut Diam Consectetur. Felis laculis Tellus Sed Sed Tellus.</p>
         </div>
       </div>
+
+      <div className="lowerBody">
+        <div className="articles">
+          <div className="container">
+            <div>
+              <img src={require('../../assets/images/Mask group (6).png')} className='captionImage' alt="image6" />
+            </div>
+            <div className="captionBody">
+              <button className='caption'>TECHNOLOGY</button>
+              <h3 className="captionHeader">Lorem Ipsum Dolor Sit Amet - Gi Consectetur. Mauris Integer Nulla Id.</h3>
+              <p className="captionText">Lorem Ipsum Dolor Sit Amet Consectetur. Orci Porta Nunc Etiam Duis Molestie Interdum Venenatis Nisl Et. Ullamcorper Lorem Magna.</p>
+            </div>
+          </div>
+
+          <div className="container">
+            <div className="captionImage">
+              <img src={require('../../assets/images/Mask group (7).png')} className='captionImage' alt="image7" />
+            </div>
+            <div className="captionBody">
+              <button className='caption'>DELIVERY</button>
+              <h3 className="captionHeader">Lorem Ipsum Dolor Sit Amet - Gi Consectetur. Mauris Integer Nulla Id.</h3>
+              <p className="captionText">Lorem Ipsum Dolor Sit Amet Consectetur. Orci Porta Nunc Etiam Duis Molestie Interdum Venenatis Nisl Et. Ullamcorper Lorem Magna.</p>
+            </div>
+          </div>
+
+          <div className="container">
+            <div className="captionImage">
+              <img src={require('../../assets/images/Mask group (8).png')} className='captionImage' alt="image8" />
+            </div>
+            <div className="captionBody">
+              <button className='caption'>AUTOMOBILE</button>
+              <h3 className="captionHeader">Lorem Ipsum Dolor Sit Amet - Gi Consectetur. Mauris Integer Nulla Id.</h3>
+              <p className="captionText">Lorem Ipsum Dolor Sit Amet Consectetur. Orci Porta Nunc Etiam Duis Molestie Interdum Venenatis Nisl Et. Ullamcorper Lorem Magna.</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="ad">
+          <img src={require('../../assets/images/Mask group (9).png')} className='adImage' alt='Ad-image' />
+          <img src={require('../../assets/logos/Dispatch hub logo 1.png')} className='adLogo' alt='Ad-logo' />
+          <p className='adText'>Lorem Ipsum Dolor Sit Amet Consectetur. Gravida Nec Nulla Diam Fames Pretium Odio Arcu. Pellentesque Risus Est Turpis Dapibus Tincidunt Odio Cursus Viverra. Id Ullamcorper Turpis Mi Mi. Fermentum Eget Massa.</p>
+          <p className='adPrint'>Ad</p>
+        </div>
+      </div>
     </div>
   )
 }
