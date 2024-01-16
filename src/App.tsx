c746c709b32d01258356f6137881c3c9b9c3f16e
Henry Tochukwu Uzor <h.uzor17@gmail.com>
2024-01-16 13:19:19 +0100

    feat: header imported and mounted in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import { Body } from './components/body/Body';
+import { Header } from './components/header/Header';
 import { Recent } from './components/recent/Recent';
 
 const App = () => {
   return (
     <div className="App">
+      <Header />
       <Body />
       <Recent />
     </div>
