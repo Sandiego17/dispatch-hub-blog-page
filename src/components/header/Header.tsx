ce93cf88337659e2cc1b3916b5e88485fe8e88a0
Henry Tochukwu Uzor <h.uzor17@gmail.com>
2024-02-02 17:19:15 +0100

    feat: header section made responsive across all screen sizes

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,24 +1,24 @@
-import './Header.css';
+import styles from './Header.module.css'
 
 export const Header = () => {
   return (
-    <div className='header'>
-      <div className='headerElements'>
-        <div className='logo'>
-          <img src={require('../../assets/logos/Dispatch hub logo 1.png')} className='blogLogo' alt='Blog Logo' />
+    <div className={styles.header}>
+      <div className={styles.headerElements}>
+        <div className={styles.logo}>
+          <img src={require('../../assets/logos/Dispatch hub logo 1.png')} className={styles.blogLogo} alt='Blog Logo' />
         </div>
 
-        <div className='searchBar'>
-          <img src={require('../../assets/icons/Search Icon.png')} className='searchIcon' alt='Search Icon' />
-          <input className='searchBarInput' type='text' placeholder='Search' />
+        <div className={styles.searchBar}>
+          <img src={require('../../assets/icons/Search Icon.png')} className={styles.searchIcon} alt='Search Icon' />
+          <input className={styles.searchBarInput} type='text' placeholder='Search' />
         </div>
 
-        <div className='menu'>
+        <button className={styles.menu}>
           <h1>Menu</h1>
-          <button className='dropdownButton'>
-            <img src={require('../../assets/icons/Dropdown.png')} className='dropdown' alt='menuDropdown' />
-          </button>
-        </div>
+          <div className={styles.dropdownButton}>
+            <img src={require('../../assets/icons/Dropdown.png')} className={styles.dropdown} alt='menuDropdown' />
+          </div>
+        </button>
       </div>
     </div>
   )
