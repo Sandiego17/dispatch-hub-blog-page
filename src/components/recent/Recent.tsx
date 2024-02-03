a007cbca0fb8b87d253e56f9a57074f5af8b7579
Henry Tochukwu Uzor <h.uzor17@gmail.com>
2024-02-03 18:22:23 +0100

    update: recent section made responsive

--- src/components/recent/Recent.tsx
+++ src/components/recent/Recent.tsx
@@ -1,38 +1,38 @@
-import './Recent.css';
+import styles from './Recent.module.css'
 
 export const Recent = () => {
   return (
-    <div className='recentArticles'>
-      <h1 className='sectionHeading'>RECENT ARTICLES</h1>
+    <div className={styles.recentArticles}>
+      <h1 className={styles.sectionHeading}>RECENT ARTICLES</h1>
 
-      <div className='recentSection'>
-        <div className='recent'>
-          <div className='recentImage'>
-            <img src={require('../../assets/images/Mask group (10).png')} className='articleImage' alt='image10' />
+      <div className={styles.recentSection}>
+        <div className={styles.recent}>
+          <div className={styles.recentImage}>
+            <img src={require('../../assets/images/Mask group (10).png')} className={styles.articleImage} alt='image10' />
           </div>
-          <div className='recentPost'>
-            <h3 className='recentHeading'>PACKAGES</h3>
-            <p className='recentText'>Lorem Ipsum + Dolor Sit Consectetur Orci Porta Nunc Etiam Duis.</p>
+          <div className={styles.recentPost}>
+            <h3 className={styles.recentHeading}>PACKAGES</h3>
+            <p className={styles.recentText}>Lorem Ipsum + Dolor Sit Consectetur Orci Porta Nunc Etiam Duis.</p>
           </div>
         </div>
 
-        <div className='recent'>
-          <div className='recentImage'>
-            <img src={require('../../assets/images/Mask group (11).png')} className='articleImage' alt='image11' />
+        <div className={styles.recent}>
+          <div className={styles.recentImage}>
+            <img src={require('../../assets/images/Mask group (11).png')} className={styles.articleImage} alt='image11' />
           </div>
-          <div className='recentPost'>
-            <h3 className='recentHeading'>FREIGHT</h3>
-            <p className='recentText'>30 Unique Lorem Ipsum Dolor Sit Amet Consectetur.</p>
+          <div className={styles.recentPost}>
+            <h3 className={styles.recentHeading}>FREIGHT</h3>
+            <p className={styles.recentText}>30 Unique Lorem Ipsum Dolor Sit Amet Consectetur.</p>
           </div>
         </div>
 
-        <div className='recent'>
-          <div className='recentImage'>
-            <img src={require('../../assets/images/Mask group (12).png')} className='articleImage' alt='image10' />
+        <div className={styles.recent}>
+          <div className={styles.recentImage}>
+            <img src={require('../../assets/images/Mask group (12).png')} className={styles.articleImage} alt='image10' />
           </div>
-          <div className='recentPost'>
-            <h3 className='recentHeading'>TRANSPORTATION</h3>
-            <p className='recentText'>Lorem Ipsum Dolor Sit Amet Orci Port Nunc Etiam Duis Molestie Interdum.</p>
+          <div className={styles.recentPost}>
+            <h3 className={styles.recentHeading}>TRANSPORTATION</h3>
+            <p className={styles.recentText}>Lorem Ipsum Dolor Sit Amet Orci Port Nunc Etiam Duis Molestie Interdum.</p>
           </div>
         </div>
       </div>
