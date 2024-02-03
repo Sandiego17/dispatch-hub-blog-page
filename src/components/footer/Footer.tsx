652b6ce753ee0d222141630d52bab6f8d263cf24
Henry Tochukwu Uzor <h.uzor17@gmail.com>
2024-02-03 18:28:36 +0100

    update: footer styles sheet made a css module

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,45 +1,45 @@
-import './Footer.css'
+import styles from './Footer.module.css'
 
 export const Footer = () => {
   return (
-    <div className='footer'>
-      <div className='footerContainer'>
-        <div className='footerBody'>
-          <div className='footerLogo'>
-            <img src={require('../../assets/logos/Logo.png')} className='footerLogo' alt='footer-logo' />
+    <div className={styles.footer}>
+      <div className={styles.footerContainer}>
+        <div className={styles.footerBody}>
+          <div className={styles.footerLogo}>
+            <img src={require('../../assets/logos/Logo.png')} className={styles.footerLogo} alt='footer-logo' />
           </div>
-          <div className='links'>
-            <div className='linkSections'>
+          <div className={styles.links}>
+            <div className={styles.linkSections}>
               <h3>Dispatch Hub</h3>
               <p>download</p>
               <p>states</p>
               <p>cities</p>
             </div>
-            <div className='linkSections'>
+            <div className={styles.linkSections}>
               <h3>Partner with us</h3>
               <p>Courier</p>
               <p>Vehicle owner</p>
             </div>
-            <div className='linkSections'>
+            <div className={styles.linkSections}>
               <h3>Company</h3>
               <p>About Dispatch Hub</p>
               <p>Blog</p>
             </div>
           </div>
         </div>
-        <div className='buttons'>
-          <div className='icons'>
-            <button><img src={require('../../assets/icons/FACEBOOK ICON 1.png')} className='facebook' alt='facebook' /></button>
-            <button><img src={require('../../assets/icons/X ICON 1.png')} className='x' alt='x' /></button>
-            <button><img src={require('../../assets/icons/INSTAGRAM ICON 1.png')} className='instagram' alt='instagram' /></button>
-            <button><img src={require('../../assets/icons/LIKEDI ICON 1.png')} className='linkedIn' alt='linkedIn' /></button>
+        <div className={styles.buttons}>
+          <div className={styles.icons}>
+            <button><img src={require('../../assets/icons/FACEBOOK ICON 1.png')} className={styles.facebook} alt='facebook' /></button>
+            <button><img src={require('../../assets/icons/X ICON 1.png')} className={styles.x} alt='x' /></button>
+            <button><img src={require('../../assets/icons/INSTAGRAM ICON 1.png')} className={styles.instagram} alt='instagram' /></button>
+            <button><img src={require('../../assets/icons/LIKEDI ICON 1.png')} className={styles.linkedIn} alt='linkedIn' /></button>
           </div>
-          <div className='track'>
+          <div className={styles.track}>
             <button>Get the App</button>
             <button>Track</button>
           </div>
         </div>
-        <div className='copyright'>
+        <div className={styles.copyright}>
           <p>&#169; 2023 Dispatch Hub</p>
           <ul>
             <li>Terms & Conditions</li>
