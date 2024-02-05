d710f47bb4eb7e0a29d9ab6796b0bd3fb44ea3b5
Henry Tochukwu Uzor <h.uzor17@gmail.com>
2024-02-05 16:32:52 +0100

    feat: page reloads when ".menu" button display changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Body } from './components/body/Body';
 import { Footer } from './components/footer/Footer';
@@ -7,6 +7,21 @@ import { Recent } from './components/recent/Recent';
 
 const App = () => {
   const [showNavButtons, setShowNavButtons] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 1500 && showNavButtons) {
+        setShowNavButtons(false);
+        window.location.reload();
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [showNavButtons]);
 
   return (
     <div className="App">
