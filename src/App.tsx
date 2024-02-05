1ceeb775839d4a18457375530bddec14f18427af
Henry Tochukwu Uzor <h.uzor17@gmail.com>
2024-02-05 15:08:52 +0100

    feat: NavBarProps used in App.tsx and page overlay defined

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import { Body } from './components/body/Body';
 import { Footer } from './components/footer/Footer';
@@ -5,9 +6,19 @@ import { Header } from './components/header/Header';
 import { Recent } from './components/recent/Recent';
 
 const App = () => {
+  const [showNavButtons, setShowNavButtons] = useState(false);
+
   return (
     <div className="App">
-      <Header />
+      <Header
+        home='Home'
+        aboutUs='About Us'
+        services='Services'
+        contactUs='Contact Us'
+        showNavButtons={showNavButtons}
+        setShowNavButtons={setShowNavButtons}
+      />
+      {showNavButtons && <div className='overlay' onClick={() => setShowNavButtons(false)}></div>}
       <Body />
       <Recent />
       <Footer />
