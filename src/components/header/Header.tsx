a9d88fb49630fef2539f8fdb24bfef355c4333e1
Henry Tochukwu Uzor <h.uzor17@gmail.com>
2024-02-05 15:07:40 +0100

    feat: passing NavBarProps

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,29 @@
+import React, { useState } from 'react';
 import styles from './Header.module.css'
 
-export const Header = () => {
+interface NavBarProps {
+  home: string;
+  aboutUs: string;
+  services: string;
+  contactUs: string;
+  showNavButtons: boolean;
+  setShowNavButtons: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export const Header = ({
+  home,
+  aboutUs,
+  services,
+  contactUs,
+  showNavButtons,
+  setShowNavButtons
+}: NavBarProps) => {
+  // const [showNavButtons, setShowNavButtons] = useState(false);
+
+  const toggleNavButtons = () => {
+    setShowNavButtons(!showNavButtons)
+  };
+
   return (
     <div className={styles.header}>
       <div className={styles.headerElements}>
@@ -13,12 +36,21 @@ export const Header = () => {
           <input className={styles.searchBarInput} type='text' placeholder='Search' />
         </div>
 
-        <button className={styles.menu}>
+        <button className={styles.menu} onClick={toggleNavButtons}>
           <h1>Menu</h1>
           <div className={styles.dropdownButton}>
             <img src={require('../../assets/icons/Dropdown.png')} className={styles.dropdown} alt='menuDropdown' />
           </div>
         </button>
+
+        <div className={`${styles.navButtons} ${showNavButtons ? styles.show : ''}`}>
+          <ul>
+            <li className={styles.navLinks}>{home}</li>
+            <li className={styles.navLinks}>{aboutUs}</li>
+            <li className={styles.navLinks}>{services}</li>
+            <li className={styles.navLinks}>{contactUs}</li>
+          </ul>
+        </div>
       </div>
     </div>
   )
