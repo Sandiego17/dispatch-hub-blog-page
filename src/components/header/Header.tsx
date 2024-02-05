aa4ce3d66a7fcfb40fef7e28672fc09bf03c37f9
Henry Tochukwu Uzor <h.uzor17@gmail.com>
2024-02-05 16:38:18 +0100

    update: Header.tsx, Body.css and Footer.module.css

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -28,18 +28,30 @@ export const Header = ({
     <div className={styles.header}>
       <div className={styles.headerElements}>
         <div className={styles.logo}>
-          <img src={require('../../assets/logos/Dispatch hub logo 1.png')} className={styles.blogLogo} alt='Blog Logo' />
+          <img
+            src={require('../../assets/logos/Dispatch hub logo 1.png')}
+            className={styles.blogLogo}
+            alt='Blog Logo'
+          />
         </div>
 
         <div className={styles.searchBar}>
-          <img src={require('../../assets/icons/Search Icon.png')} className={styles.searchIcon} alt='Search Icon' />
+          <img
+            src={require('../../assets/icons/Search Icon.png')}
+            className={styles.searchIcon}
+            alt='Search Icon'
+          />
           <input className={styles.searchBarInput} type='text' placeholder='Search' />
         </div>
 
         <button className={styles.menu} onClick={toggleNavButtons}>
           <h1>Menu</h1>
           <div className={styles.dropdownButton}>
-            <img src={require('../../assets/icons/Dropdown.png')} className={styles.dropdown} alt='menuDropdown' />
+            <img
+              src={require('../../assets/icons/Dropdown.png')}
+              className={styles.dropdown}
+              alt='menuDropdown'
+            />
           </div>
         </button>
 
